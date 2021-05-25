181fec3d8fd65883c3c40060bdd48d751aecfed4
Alvin Novian <alvinnovian883@gmail.com>
2021-05-25 17:16:12 +0700

    feat(nextjs): init tasks

--- src/tasks/nextjs/index.js
+++ src/tasks/nextjs/index.js
@@ -1,7 +1,21 @@
 const cp = require("child_process");
+const inquirer = require("inquirer");
 
-const setupNextjs = () => {
-  cp.exec("mkdir nextjs");
+const setupNextjs = async () => {
+  const questionsPrompt = await inquirer.prompt([
+    {
+      type: "input",
+      name: "folder_name",
+      message: "What's the project name?",
+      default: "nextjs-project",
+    },
+  ]);
+
+  cp.execSync(`mkdir ${questionsPrompt.folder_name}`);
+  cp.execSync(`git clone https://github.com/alvin883/nextjs-setup.git .`, {
+    stdio: "inherit",
+    cwd: questionsPrompt.folder_name,
+  });
 };
 
 module.exports = setupNextjs;
